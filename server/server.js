380b888eaf0f8c622de735c19c53b2704c4957db
LeydyK93 <leydykm93@gmail.com>
2019-02-18 23:03:03 -0400

    feat: add login and index.js for configuration routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+
 require('./config/config')
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -11,12 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-app.use(require('./routes/usuarios'))
+// configuracion global de rutas
+app.use(require('./routes/index'))
 mongoose.connect(process.env.URLDB, (err, res)=>{
     if(err)  throw err;
-
-    console.log("Base de datos ONLINE")
     
+    console.log("Base de datos ONLINE")
 });
  
 app.listen(process.env.PORT, ()=>{ console.log(`Escuchando por el puerto ${process.env.PORT}`)})
